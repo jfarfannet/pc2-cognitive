5f9466557e5fd848c326d93dac4ec4f36e4304d1
Jaime Farfan <jfarfannet@gmail.com>
2022-05-28 08:38:21 -0500

    primer commit

---
+++ routes/usuarios.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+var usuario = require('../controllers/UsuarioController');
+
+router.get('/',usuario.list);
+router.get('/show/:id', usuario.show);
+router.get('/edit/:id', usuario.edit);
+router.post('/update/:id', usuario.update);
+router.post('/delete/:id', usuario.delete);
+router.get('/create', usuario.create);
+router.post('/save', usuario.save);
+
+module.exports = router;
